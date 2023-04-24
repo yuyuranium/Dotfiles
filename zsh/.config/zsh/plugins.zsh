df4f9c3a226a8165d4fe93c09564f3d36496e5f8
yuyuranium <n28111534@gs.ncku.edu.tw>
2023-04-24 20:54:57 +0800

    Set history search for different OS

--- zsh/.config/zsh/plugins.zsh
+++ zsh/.config/zsh/plugins.zsh
@@ -1,7 +1,21 @@
 # zsh-history-substring-search
 source "$ZAP_PLUGIN_DIR/zsh-history-substring-search/zsh-history-substring-search.zsh"
 
-bindkey '^[[A' history-substring-search-up
-bindkey '^[[B' history-substring-search-down
+case "$(uname -s)" in
+  Darwin)
+    bindkey '^[[A' history-substring-search-up
+    bindkey '^[[B' history-substring-search-down
+    ;;
+
+  Linux)
+    bindkey "$terminfo[kcuu1]" history-substring-search-up
+    bindkey "$terminfo[kcud1]" history-substring-search-down
+    ;;
+
+  *)
+    # echo 'Other OS'
+    ;;
+esac
+
 
 export HISTORY_SUBSTRING_SEARCH_ENSURE_UNIQUE=1
