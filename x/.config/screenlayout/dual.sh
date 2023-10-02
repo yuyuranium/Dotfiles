4bd3de166a2ea514c389bbf2fd93711fe0497974
yuyuranium <n28111534@gs.ncku.edu.tw>
2023-10-02 16:36:29 +0800

    Update font face and screen layout

--- x/.config/screenlayout/dual.sh
+++ x/.config/screenlayout/dual.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DP-1 --mode 1920x1200 --pos 0x0 --rotate left --output HDMI-1 --off --output HDMI-2 --primary --mode 1920x1200 --pos 1200x221 --rotate normal
+xrandr --output DP-1 --mode 1920x1200 --pos 0x0 --rotate left --output HDMI-1 --off --output HDMI-2 --primary --mode 1920x1080 --pos 1200x332 --rotate normal
