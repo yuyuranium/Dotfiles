df9c5a287c6ba5bf9fa9b01441f13db17210abac
yuyuranium <n28111534@gs.ncku.edu.tw>
2023-11-11 13:53:10 +0800

    Add alias for cic stuffs

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -14,21 +14,47 @@ alias free='free -m' # show sizes in MB
 
 case "$(uname -s)" in
 
-Darwin)
-    # echo 'Mac OS X'
-    alias ls='ls -G'
-    ;;
+    Darwin)
+        # echo 'Mac OS X'
+        alias ls='ls -G'
+        ;;
 
-Linux)
-    alias ls='ls --color=auto'
-    alias ll='ls -l'
-    alias l='ls -la'
-    ;;
+    Linux)
+        alias ls='ls --color=auto'
+        alias ll='ls -l'
+        alias l='ls -la'
+        if [[ -d /opt/CIC ]]; then
+            alias cic-mount='
+            sudo mount -t nfs CASGPU:VOL1/Shared/CIC /opt/CIC -o ro
+            '
+        else
+            alias 'cic-mount'='
+            sudo mkdir -p /opt/CIC
+            sudo mount -t nfs CASGPU:VOL1/Shared/CIC /opt/CIC -o ro
+            sudo mkdir -p /usr/cad/CBDK
+            sudo ln -s /opt/CIC/Cell_Libraries/CBDK018_UMC_Faraday_v1.0 /usr/cad/CBDK
+            sudo ln -s /opt/CIC/Cell_Libraries/CBDK_IC_Contest_v2.1 /usr/cad/CBDK
+            sudo ln -s /opt/CIC/Cell_Libraries/CBDK_IC_Contest_v2.5 /usr/cad/CBDK
+            '
+        fi
 
-CYGWIN* | MINGW32* | MSYS* | MINGW*)
-    # echo 'MS Windows'
-    ;;
-*)
-    # echo 'Other OS'
-    ;;
+        alias 'cic-start'='
+        source /opt/CIC/exec/vcs.shrc
+        source /opt/CIC/exec/Verdi.shrc
+        source /opt/CIC/exec/Synthesis.shrc
+        source /opt/CIC/exec/IES.shrc
+        source /opt/CIC/exec/Jasper.shrc
+        source /opt/CIC/exec/Innovus.shrc
+        source /opt/CIC/exec/Primetime.shrc
+        '
+
+        alias 'cic-stop'='unset LD_LIBRARY_PATH'
+        ;;
+
+    CYGWIN* | MINGW32* | MSYS* | MINGW*)
+        # echo 'MS Windows'
+        ;;
+    *)
+        # echo 'Other OS'
+        ;;
 esac
